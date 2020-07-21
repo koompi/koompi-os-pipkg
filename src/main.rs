1046ba0ed8ec7131d2517069bb31ba796e50b007
mrrbrilliant <mrrbrilliantwork@gmail.com>
2020-07-21 09:11:59 +0700

    added concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
-#![allow(non_snake_case)]
+#![allow(non_snake_case, unused_imports, unused_assignments, unused_variables)]
 
 mod interfaces;
 mod schemas;
 
+use rayon::prelude::*;
 use schemas::desc_file::DescFile;
+use std::fs;
+use std::thread;
+use std::time::{Duration, Instant};
 
 fn main() {
-    let mut package_info: DescFile = DescFile::new();
-    package_info.import("desc");
-    package_info.export().unwrap_or(());
+    // Generate json description file
+    // let mut package_info: DescFile = DescFile::new();
+    // package_info.import("desc").export().unwrap_or(());
+    let mut all_file: Vec<String> = Vec::new();
+    let start = Instant::now();
+    let paths = fs::read_dir("/home/brilliant/Documents/test/pacman/sync/").unwrap();
+    for path in paths {
+        let file_path: String = format!("{}/desc", path.unwrap().path().display());
+        all_file.push(file_path.clone());
+    }
+
+    all_file.par_iter().for_each(|n| {
+        let mut package_info: DescFile = DescFile::new();
+        package_info.import(n).export().unwrap_or(());
+        // true
+    });
+
+    // for path in paths {
+    //     let file_path = &format!("{}/desc", path.unwrap().path().display());
+    //     let mut package_info: DescFile = DescFile::new();
+    //     package_info.import(file_path).export().unwrap_or(());
+    // }
+    let duration = start.elapsed();
+
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
 }
